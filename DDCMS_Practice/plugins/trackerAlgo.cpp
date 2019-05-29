d75d28c4ba3d38c4778cd753e1a4060b4d27dd74
Raman Sehgal <raman.sehgal@cern.ch>
2019-05-29 17:43:49 +0530

    Working example of placement of tube inside polycone. make use of xml and detector constructor with Algorithm.
    But working only after putting these files along with DDCMS example
    but NOT standalone.

--- DDCMS_Practice/plugins/trackerAlgo.cpp
+++ DDCMS_Practice/plugins/trackerAlgo.cpp
@@ -13,14 +13,39 @@ using namespace std;
 using namespace dd4hep;
 using namespace dd4hep::cms;
 
-//From DD4hep
 static long algorithm(Detector& description, ParsingContext& ctxt, xml_h e, SensitiveDetector& /* sens */){
 	PlacedVolume  pv;
 	Namespace     ns(ctxt, e, true);
 	AlgoArguments args(ctxt, e);
-	//string        parentName = args.parentName();
-	//Volume         mother      = ns.volume(args.parentName());
+	string        parentName = args.parentName();
+	Volume         mother      = ns.volume(args.parentName());
+	std::string              material   = args.value<std::string>("Material");
+	dd4hep::Material matter = ns.material(material);
+	std::cout << "===================================================================" << std::endl;
+	std::cout << "Mother Name : " << mother.name() << " : Material : " << material << std::endl;
+	std::cout << "===================================================================" << std::endl;
+	std::vector<std::string> innerTubeVector    = args.value<std::vector<std::string> >("InnerTubeVector");
+	for (unsigned int i=0; i<innerTubeVector.size(); ++i) {
+		if(i==0){
+		dd4hep::Solid solid = dd4hep::Tube(50., 70, 180.);
+		ns.addSolidNS(ns.prepend(innerTubeVector[i]), solid);
+		dd4hep::Volume glog1 = dd4hep::Volume(solid.name(), solid, matter);
+		mother.placeVolume(glog1);
+		ns.addVolumeNS(glog1);
+		}
+		if(i==1){
+                dd4hep::Solid solid = dd4hep::Tube(30., 40, 220.);
+                ns.addSolidNS(ns.prepend(innerTubeVector[i]), solid);
+                dd4hep::Volume glog1 = dd4hep::Volume(solid.name(), solid, matter);
+                //mother.placeVolume(glog1);
+                ns.addVolumeNS(glog1);
+                }
+		
 
+	
+	}
+
+	std::cout << "Raman................................" << std::endl;
 	return 1;
 }
 
