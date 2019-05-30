0cb05e3d0b1c34c6ff956d6561b4a601c78abb88
Raman Sehgal <raman.sehgal@cern.ch>
2019-05-30 15:28:27 +0530

    Modification to understand how to define some placement in xml and some placement in detectorconstructor

--- DDCMS_Practice/plugins/trackerAlgo.cpp
+++ DDCMS_Practice/plugins/trackerAlgo.cpp
@@ -25,22 +25,39 @@ static long algorithm(Detector& description, ParsingContext& ctxt, xml_h e, Sens
 	std::cout << "Mother Name : " << mother.name() << " : Material : " << material << std::endl;
 	std::cout << "===================================================================" << std::endl;
 	std::vector<std::string> innerTubeVector    = args.value<std::vector<std::string> >("InnerTubeVector");
+	dd4hep::Volume glog1 ;
+	dd4hep::Rotation3D rotation;
 	for (unsigned int i=0; i<innerTubeVector.size(); ++i) {
 		if(i==0){
 		dd4hep::Solid solid = dd4hep::Tube(50., 70, 180.);
 		ns.addSolidNS(ns.prepend(innerTubeVector[i]), solid);
-		dd4hep::Volume glog1 = dd4hep::Volume(solid.name(), solid, matter);
+		//dd4hep::Volume 
+		/*
+		** Transformation of InnerTube1 will be taken from trackerAlgo.xml file
+		*/
+		glog1 = dd4hep::Volume(solid.name(), solid, matter);
 		//mother.placeVolume(glog1);
 		ns.addVolumeNS(glog1);
 		}
 		if(i==1){
                 dd4hep::Solid solid = dd4hep::Tube(30., 40, 220.);
                 ns.addSolidNS(ns.prepend(innerTubeVector[i]), solid);
-                dd4hep::Volume glog1 = dd4hep::Volume(solid.name(), solid, matter);
+                dd4hep::Volume glog2 = dd4hep::Volume(solid.name(), solid, matter);
                 //mother.placeVolume(glog1);
+                /*
+                ** Transformation of InnerTube2 is not defined in trackerAlgo.xml and is hardcoded here
+                */
+                glog1.placeVolume(glog2,dd4hep::Transform3D(rotation,dd4hep::Position(0.,0.,100.)));
                 ns.addVolumeNS(glog1);
                 }
 		
+		/*
+		** A point worth noting, that placement must be defined only at one place 
+		** ie. either in XML file or in the DetectorConstructor.
+		**
+		** Like in the present case, Placement of InnerTube1 is defined in XML file
+		** but placement for InnerTube2 is defined as shown above in DetectorConstructor
+		*/
 
 	
 	}
